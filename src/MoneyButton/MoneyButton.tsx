ad653e0e93b7bc3f49686a0aeb431d7fe5d95ce9
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2024-07-15 21:08:13 +0800

    feat: refine money button

--- src/MoneyButton/MoneyButton.tsx
+++ src/MoneyButton/MoneyButton.tsx
@@ -1,13 +1,20 @@
-import { PropsWithChildren } from "react";
+import { ButtonHTMLAttributes } from "react";
 import styles from "./MoneyButton.module.css";
 
-export default function MoneyButton(props: PropsWithChildren) {
+export default function MoneyButton(props: ButtonHTMLAttributes<HTMLButtonElement>) {
+	const { children, ...rest } = props;
 	return (
-		<button className={styles.money}>{props.children}
+		<button className={styles.money} {...rest}>{props.children}
 			<span className={styles.background}>
-				{new Array(10).fill(0).map((_, idx) => <i key={idx}></i>)}
+				{new Array(10).fill(0).map((_, idx) =>
+					<i key={idx}
+						style={{
+							position: 'absolute',
+							top: `${Math.random() * 100}%`,
+							left: `${Math.random() * 100}%`,
+							transform: `rotate(${Math.floor(Math.random() * 180)}deg)`
+						}}></i>)}
 			</span>
 		</button>
-
 	)
 }
