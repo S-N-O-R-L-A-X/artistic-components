8bf382f461be2bdf6888f866041bd411c21b912c
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2024-08-11 14:43:07 +0800

    feat: init Octocat

---
+++ src/GithubCorner/Octocat.stories.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Octocat from "./Octocat";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta: Meta<typeof Octocat> = {
+	title: 'Octocat',
+	component: Octocat,
+	args: {
+		width: 800,
+		height: 600,
+		backgroundColor: "black"
+	}
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+	name: 'example',
+	render: (args) => <Octocat {...args} />
+};
