03385eea452ddc8e4e8eeb4d4bd7c7add5128fbb
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2024-10-01 14:07:19 +0800

    feat: add props for background image setting

--- src/SpotlightText/SpotlightText.tsx
+++ src/SpotlightText/SpotlightText.tsx
@@ -1,15 +1,28 @@
-import { HTMLAttributes } from "react";
+import { CSSProperties, HTMLAttributes } from "react";
 import styles from "./SpotlightText.module.css";
 
 export interface SpotlightTextProps extends HTMLAttributes<HTMLDivElement> {
 	text?: string;
+	backgroundImageURL?: string;
+	colors?: {
+		startColor: string;
+		middleColor?: string;
+		endColor: string;
+	}
 }
 
 export default function SpotlightText(props: SpotlightTextProps) {
-	const { className, text = "Hello World!", ...rest } = props;
-	return (
-		<div data-spotlight={text} className={styles.spotlight + " " + className} {...rest}>
-			{text}
-		</div>
-	)
+	const { backgroundImageURL = "https://images.unsplash.com/photo-1579547621869-0ddb5f237392?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80", colors, className, text = "Hello World!", ...rest } = props;
+	return colors ?
+		(
+			<div data-spotlight={text} className={styles.spotlight + " " + styles['spotlight-color'] + className} {...rest}>
+				{text}
+			</div>
+		)
+		:
+		(
+			<div data-spotlight={text} style={{ "--background-image": `url(${backgroundImageURL})` } as CSSProperties} className={styles.spotlight + " " + className} {...rest}>
+				{text}
+			</div >
+		)
 }
