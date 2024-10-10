d93ec07b44f35596b40b36609f54edce7489b46d
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2024-10-10 20:11:30 +0800

    feat: add props for colors

--- src/SpotlightText/SpotlightText.tsx
+++ src/SpotlightText/SpotlightText.tsx
@@ -4,25 +4,30 @@ import styles from "./SpotlightText.module.css";
 export interface SpotlightTextProps extends HTMLAttributes<HTMLDivElement> {
 	text?: string;
 	backgroundImageURL?: string;
-	colors?: {
-		startColor: string;
-		middleColor?: string;
-		endColor: string;
-	}
+	colors?: string[];
 }
 
 export default function SpotlightText(props: SpotlightTextProps) {
 	const { backgroundImageURL = "https://images.unsplash.com/photo-1579547621869-0ddb5f237392?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80", colors, className, text = "Hello World!", ...rest } = props;
-	return colors ?
-		(
-			<div data-spotlight={text} className={styles.spotlight + " " + styles['spotlight-color'] + className} {...rest}>
+	if (colors && colors.length) {
+		return (
+			<div data-spotlight={text}
+				className={styles.spotlight + " " + styles['spotlight-color'] + " " + className}
+				style={{ "--spotlight-colors": colors.join(', ') } as CSSProperties}
+				{...rest}
+			>
 				{text}
 			</div>
 		)
-		:
-		(
-			<div data-spotlight={text} style={{ "--background-image": `url(${backgroundImageURL})` } as CSSProperties} className={styles.spotlight + " " + className} {...rest}>
-				{text}
-			</div >
-		)
+	}
+
+	return (
+		<div data-spotlight={text}
+			className={styles.spotlight + " " + styles['spotlight-image'] + " " + className}
+			style={{ "--spotlight-image": `url(${backgroundImageURL})` } as CSSProperties}
+			{...rest}
+		>
+			{text}
+		</div >
+	)
 }
